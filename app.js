965b9fec99e86265e14884dcd0105e7e6e81979e
vish9151 <84173784+vishal9151@users.noreply.github.com>
2023-10-21 15:30:00 +0530

    updated

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.use(
     secret: process.env.SECRET_Id,
     resave: false,
     saveUninitialized: false,
-    // cookie: {
-    //   secure: process.env.NODE_ENV === "development" ? false : true,
-    //   httpOnly: process.env.NODE_ENV === "development" ? false : true,
-    //   sameSite: process.env.NODE_ENV === "development" ? false : "none",
-    // },
+    cookie: {
+      // secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: false,
+      // sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    },
   })
 );
 
